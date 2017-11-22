4114a6e4dec3c887df81b3bcec3fd6590e39faa1
Paulo Coelho Alves <paulo@pcalv.es>
2017-11-22 17:55:33 +0000

    Bumped tv to 3.30.0-bcae8a0b

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class PlexMediaPlayer(ConanFile):
   generators = "cmake"
 
   def requirements(self):
-    self.requires("web-client-tv2/3.28.0-f8b4c1e6@plex/public")
+    self.requires("web-client-tv2/3.30.0-bcae8a0b@plex/public")
 
     if self.options.include_desktop:
       self.requires("web-client-desktop/3.26.2-3d9c616@plex/public")
